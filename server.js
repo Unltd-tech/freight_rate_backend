e61fed6a0e09463c0b697cf955d38912668b954d
Hadiya <hadiya@unltddevice.com>
2026-06-03 14:30:28 +0300

    server.js updated

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ const multer = require("multer");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://calculator.qbh.qa",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -201,6 +208,7 @@ app.post(
   },
 );
 
-app.listen(5000, () => {
-  console.log("🚀 Server running on http://localhost:5000");
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
